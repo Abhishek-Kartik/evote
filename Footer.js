a453b0c66592223dbc162f8919ef939cec4bd9f4
Abhishek Kumar <73778532+Abhishek-Kartik@users.noreply.github.com>
2022-05-19 00:24:08 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import "./Footer.css";
+
+const Footer = () => (
+  <>
+    <div className="footer-block"></div>
+    <div className="footer">
+      <div className="footer-container">
+        <p>
+          Student of {" "}
+          {/* <a
+            className="profile"
+            href="xxxxxxxxxxxxxxxxx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+           
+          </a> */}
+          Chandigarh University.
+        </p>
+        <p>
+          Made with by{" "}
+          {/* <a
+            className="profile"
+            href="xxxxxxxxxxxxxxxxxx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          </a> */}
+          Abhishek, Saurabh, Pranav.
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Footer;
